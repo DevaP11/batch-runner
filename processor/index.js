48f678758513561494d4452f31aae7dbe497fcc8
Deva Pramod <112617358+DevaP11@users.noreply.github.com>
2024-12-03 11:53:04 +0530

    Update Template solution

--- processor/index.js
+++ processor/index.js
@@ -4,36 +4,13 @@ const path = require('path')
 const configuration = yaml.load(fs.readFileSync(path.join(__dirname, '/../configuration.yaml'), 'utf8'))
 
 const { logger } = require('../helper/server-logger')
-const Helper = require('./helper')
 
 const processor = async (record) => {
   logger.debug('Inside Processor', record)
 
   const recordId = record?.[configuration.INDEX_NAME]
   logger.debug('Record Id', { recordId })
-
-  const email = record?.email
-  const phoneNumber = '+4400000000'
-
-  await Helper.doCognitoSignup({ email, password: 'Password@1', phoneNumber })
-  await Helper.cognitoConfirmUser(email)
-
-  /** Check If User Exists in db */
-  const { doesAccountExist } = await Helper.checkIfUserExists(email)
-
-  if (!doesAccountExist) {
-    const { marketingId } = await Helper.createMarketingUser({ email })
-    await Helper.saveUserDetailsToDb({ email, phoneNumber, marketingId }) /** If Not, Create User in ConsumerTable */
-  }
-
-  const { isProfileAlreadyCreated } = await Helper.checkIfProfileWasAlreadyCreated(email)
-
-  if (!isProfileAlreadyCreated) {
-    const { recommendationUserId } = await Helper.createRecommendationEngineUser(email)
-    await Helper.createDefaultProfile(email, recommendationUserId)
-  }
-
-  await Helper.provisionTmDemoEntitlement(email)
+  await checkpoint(email, 'Phase Name', 'Success')
 }
 
 module.exports = {
